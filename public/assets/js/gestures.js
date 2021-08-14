9e622ff5955c76341d3ddb39f76c52109ea07079
Neeha Kurelli <nkurelli@gmail.com>
2021-08-14 18:46:44 +0000

    profile

--- public/assets/js/gestures.js
+++ public/assets/js/gestures.js
@@ -1,11 +1,17 @@
 window.onload = (event) => {
-    console.log("window loaded")
+    console.log("window loaded part 2")
   // Use this to retain user state between html pages.
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log('Logged in as: ' + user.displayName);
       const googleUserId = user.uid;
       setUpUI(user);
+      document.querySelector("#name").innerHTML = user.displayName;
+      document.querySelector("#imagestuff").innerHTML= `<img alt="..." width="130" class="rounded mb-2 img-thumbnail" src=${user.photoURL}></img>`
+    //   document.querySelector("#userDropdown").innerHTML+=`<img class="img-profile rounded-circle" src="${user.photoURL}">`;
+    //   document.querySelector("#dash").innerHTML+=`<img class="img-radius" alt="User-Profile-Image" src="${user.photoURL}">`;
+      document.querySelector("#email").innerHTML = user.email;
+    //   document.querySelector("#emailV").innerHTML = user.providerId;
     } else {
         setUpUI();
       // If not logged in, navigate back to login page.

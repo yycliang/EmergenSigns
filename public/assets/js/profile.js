8405407a1a3d9d407b20c974512519d4f8987d61
Neeha Kurelli <nkurelli@gmail.com>
2021-08-15 14:32:02 +0000

    lots and lots

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,3 +1,4 @@
+var count = 0;
 window.onload = (event) => {
     console.log("window loadeds")
     // Use this to retain user state between html pages.
@@ -14,6 +15,7 @@ window.onload = (event) => {
     //   document.querySelector("#dash").innerHTML+=`<img class="img-radius" alt="User-Profile-Image" src="${user.photoURL}">`;
       document.querySelector("#email").innerHTML = user.email;
        accessProfile(user.displayName);
+       fetchProgress(user.displayName);
         } else {
             setUpUI();
         // If not logged in, navigate back to login page.
@@ -69,6 +71,48 @@ function showstuff(useriddd){
             cards += `<div class="col-lg-6 mb-2 pr-lg-1"><img src="${note.image}" alt="" class="img-fluid rounded shadow-sm"></div>`
         }
         document.querySelector("#gestures").innerHTML = cards;
+        
+    });
+}
+function fetchProgress(userName){
+    console.log("Access profile")
+    var useriddd = "";
+    const notesRef2 = firebase.database().ref(`users/`);
+    notesRef2.on("value", snapshot => {
+        console.log("notesref in users called")
+        const data2 = snapshot.val();
+        for (const noteItem in data2) {
+            console.log("started");
+            const note = data2[noteItem];
+            console.log(noteItem);
+            if (userName === note.name) {
+                found = true;
+                useriddd=noteItem;
+                console.log("found");
+                showstuff2(useriddd);
+                break;
+                
+            }
+        }
+    });
+}
+function showstuff2(useriddd){
+
+    console.log("hello" + useriddd)
+    const notesRef3 = firebase.database().ref(`users/${useriddd}/completedGestures/`);
+
+    notesRef3.on("value", snapshot => {
+        console.log("savetopics 234234 value called")
+        const data2 = snapshot.val();
+        let cards = "";
+        for (const noteItem in data2) {
+            count++;
+        }
+        console.log(Math.round(count/44*100));
+
+        document.querySelector("#bar").innerHTML = `<div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="${Math.round(count/44*100)}" aria-valuemin="0" aria-valuemax="100" style="width:${Math.round(count/44*100)}%">
+                            ${Math.round(count/44*100)}%
+                        </div>`;
         
     });
 }
